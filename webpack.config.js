56f0c547f80c4d3993dfd1ac2722acff0532a301
zhujiajia <772978409@qq.com>
2018-03-29 00:08:15 +0800

    webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,10 @@
 const path = require('path');
 
 module.exports = {
-    entry: ['whatwg-fetch', './src/index.js'],
+    // entry: ['whatwg-fetch', './src/index.js'],
+    entry: {
+        app: ['whatwg-fetch', './src/index.js']
+    },
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist'),
